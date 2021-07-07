db440ea91d784644f8533ca47fa59f07bfbb05fe
meenahoda <meena.hoda@wmfs.net>
2021-07-07 09:55:35 +0100

    style: standard

--- test/map-builder-test.js
+++ test/map-builder-test.js
@@ -20,7 +20,7 @@ const tests = [
       ]
     },
     quasar: [
-      '<q-map :id="\'minimal\'" :locked="false" :draggable="false">',
+      '<q-map :id="\'minimal\'" :locked="false" :draggable="false">'
       // '<q-map-circle format="OSGridRef" :x="data?.actualX" :y="data?.actualY" label="Minimal"',
       // ':showMarker="true" @x="v => { data.actualX = v }" @y="v => { data.actualY = v }"'
     ]
@@ -42,7 +42,7 @@ const tests = [
       ]
     },
     quasar: [
-      '<q-map :id="\'minimal\'" :locked="false" :draggable="false">',
+      '<q-map :id="\'minimal\'" :locked="false" :draggable="false">'
       // '<q-map-circle format="OSGridRef" :x="data?.actualX" :y="data?.actualY" label="Minimal"',
       // ':showMarker="true" locked="true">'
     ]
@@ -88,7 +88,7 @@ describe('Input.Map builder', () => {
       ]
     }
 
-    const expectedQuasar = "<div><q-map :id=\"\'minimal\'\" :locked=\"false\" :draggable=\"false\"><q-map-circle :data=\"data\" :m=\"{ format: \'OSGridRef\',\n  x: \'actualX\',\n  y: \'actualY\',\n  label: \'Minimal\' }\" format=\"OSGridRef\" :x=\"data?.actualX\" :y=\"data?.actualY\" :showMarker=\"true\" :showLaunches=\"false\" :launches=\"[]\" :label=\"[ \'Minimal\' ]\" @x=\"v => { data.actualX = v }\" @y=\"v => { data.actualY = v }\"></q-map-circle></q-map></div>"
+    const expectedQuasar = "<div><q-map :id=\"'minimal'\" :locked=\"false\" :draggable=\"false\"><q-map-circle :data=\"data\" :m=\"{ format: 'OSGridRef',\n  x: 'actualX',\n  y: 'actualY',\n  label: 'Minimal' }\" format=\"OSGridRef\" :x=\"data?.actualX\" :y=\"data?.actualY\" :showMarker=\"true\" :showLaunches=\"false\" :launches=\"[]\" :label=\"[ 'Minimal' ]\" @x=\"v => { data.actualX = v }\" @y=\"v => { data.actualY = v }\"></q-map-circle></q-map></div>"
 
     const quasar = InputMapBuilder(cardscript, { })
     expect(quasar).to.equal(expectedQuasar)
