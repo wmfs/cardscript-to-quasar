3a0c5c4b66cba9cf02f3cbe1ca67feb7a4c49ee4
Jasvinder Bahra <jas.bahra@wmfs.net>
2024-03-25 09:31:45 +0000

    feat((pin 262) crr dd): [sc-22942] FSI Note order for 'View all FSI notes and Actions ' (#349)
    * feat: [sc-22942] changed positioning of timeline entry launches menu, added badge topSpacing config
    
    * test: [sc-22942] added construction tests for TimelineEntry
    
    * refactor: [sc-22942] minor refactor and added badge construction tests

---
+++ test/timeline-entry-test.js
@@ -0,0 +1,32 @@
+/* eslint-env mocha */
+
+'use strict'
+const TimelineEntry = require('./../lib/builders/TimelineEntry')
+const chai = require('chai')
+const expect = chai.expect
+
+describe('timeline entry construction tests', function () {
+  it('just a title', function () {
+    const timelineEntry = TimelineEntry({
+      icon: null,
+      color: null,
+      title: 'TITLE',
+      subtitle: null,
+      showWhen: null,
+      showLaunches: null
+    }, null)
+    expect(timelineEntry).to.eql('<q-timeline-entry><template v-slot:title><div class="row"><div class="col">TITLE</div></div></template><template v-slot:subtitle></template>')
+  })
+
+  it('title and subtitle', function () {
+    const timelineEntry = TimelineEntry({
+      icon: null,
+      color: null,
+      title: 'TITLE',
+      subtitle: 'SUBTITLE',
+      showWhen: null,
+      showLaunches: null
+    }, null)
+    expect(timelineEntry).to.eql('<q-timeline-entry><template v-slot:title><div class="row"><div class="col">TITLE</div></div></template><template v-slot:subtitle>SUBTITLE</template>')
+  })
+})
